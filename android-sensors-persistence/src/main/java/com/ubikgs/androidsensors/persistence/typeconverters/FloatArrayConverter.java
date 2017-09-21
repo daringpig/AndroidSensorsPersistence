377a16b87b32b5cb7bab38063e3e26a0145fa61c
Alberto Gonzalez Perez <algonzal@uji.es>
2017-09-21 20:36:10 +0200

    FloatArrayConverter that allows float[] serialization

---
+++ android-sensors-persistence/src/main/java/com/ubikgs/androidsensors/persistence/typeconverters/FloatArrayConverter.java
@@ -0,0 +1,53 @@
+package com.ubikgs.androidsensors.persistence.typeconverters;
+
+import android.arch.persistence.room.TypeConverter;
+
+import java.util.Arrays;
+
+/**
+ * Copyright 2017 Alberto González Pérez
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * <p>http://www.apache.org/licenses/LICENSE-2.0</p>
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class FloatArrayConverter extends ArrayConverter {
+
+    @TypeConverter
+    public String serialize(float[] array) {
+        if (array == null) return null;
+        return Arrays.toString(array);
+    }
+
+    @TypeConverter
+    public float[] deserialize(String arrayAsString) {
+        if (arrayAsString == null) return null;
+
+        try {
+            return parseArray(arrayAsString);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private float[] parseArray(String arrayAsString) {
+        String[] parts = getStringParts(arrayAsString);
+
+        float[] array = new float[parts.length];
+
+        for (int i = 0; i < parts.length; i++) {
+            array[i] = Float.parseFloat(parts[i]);
+        }
+
+        return array;
+    }
+}
